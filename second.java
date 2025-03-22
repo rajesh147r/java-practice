b6d46155802b400527e5be6f075d3cdaf39bf2ab
Rajesh Uda <rajeshuda147r@gmail.com>
2025-03-22 22:17:01 +0530

    added first java file

---
+++ second.java
@@ -0,0 +1,10 @@
+public class second {
+    public static void main(String[] args) {
+        int a=10;
+        System.out.println(a);
+        System.out.print("\nHello World!!");
+        /*hello World
+        dgfgfg */
+
+    }
+}
